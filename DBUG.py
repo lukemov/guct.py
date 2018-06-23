5b4a9bca431f59dbf461e8044fb4bfddf6da5a46
lukemov <40528985+lukemov@users.noreply.github.com>
2018-06-23 20:08:58 -0300

    Update and rename .gitignore to DBUG.py

---
+++ DBUG.py
@@ -0,0 +1,113 @@
+#-*-coding:utf8;-*-
+#qpy:3
+#qpy:console
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print ("                             ")
+print ("                             ")
+print ("                             ")
+print ("                             ")
+print ("   █▀▀▄ █▀▀▄ █░░█ █▀▀▀")
+print ("   █░▒█ █▀▀▄ █░░█ █░▀█")
+print ("   █▄▄▀ ▀▀▀   ▀▀▀ ▀▀▀▀")
+print ("                 beta versão")
+print (" ")
+print (" ") #Dbug version beta 
+print (" ")
+print (" ")
+print (" ")
+print (" ")
+print ("By Lukemov")
+print (" ")
+print (" ")
+print (" ")
+
+print (' _____ Responsabilizo? _____')
+print ('             -.             ')
+print ('             -.             ')
+print ('             -.             ')
+print ('    nao me responsabilizo ')
+print ('    pelo seus atos!!')
+print ('    a script esta em ')
+print ('    versão beta pode')
+print ('    aparecer alguns erros.')
+print ('             -.             ')
+print ('             -.             ')
+print (' ____________--_____________ ')
+print ("                             ")
+ip = input ("→ ip do celular da vítima: ")
+
+if s.connect_ex(('127.0.0.7', 80)):
+    print (" ip esta sendo atacado")
+else:
+    print (" esse ip nao existe")
+
+
+
+bot = range (5, 1000, 5)
+
+x = 666
+
+while x < 677:
+    print (x,"esta sendo transportado")
+
+
+
+# 
+# Translations
+*.mo
+*.pot
+
+# Django stuff:
+*.log
+local_settings.py
+db.sqlite3
+
+# Flask stuff:
+instance/
+.webassets-cache
+
+# Scrapy stuff:
+.scrapy
+
+# Sphinx documentation
+docs/_build/
+
+# PyBuilder
+target/
+
+# Jupyter Notebook
+.ipynb_checkpoints
+
+# pyenv
+.python-version
+
+# celery beat schedule file
+celerybeat-schedule
+
+# SageMath parsed files
+*.sage.py
+
+# Environments
+.env
+.venv
+env/
+venv/
+ENV/
+env.bak/
+venv.bak/
+
+# Spyder project settings
+.spyderproject
+.spyproject
+
+# Rope project settings
+.ropeproject
+
+# mkdocs documentation
+/site
+
+# mypy
+.mypy_cache/
